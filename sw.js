b3e7d64459c9a51c746631234a15bcff6ab8ec02
kristyanne496-jpg <kristyanne496@gmail.com>
2026-01-17 16:32:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
-const cacheName = 'cardapio-v1';
+const CACHE_NAME = 'cardapio-v28-cache';
 const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
+
+// Lógica para enviar notificação em segundo plano
+self.addEventListener('periodicsync', (event) => {
+  if (event.tag === 'check-alarms') {
+    // Aqui o sistema pode disparar notificações mesmo fechado
+  }
+});
+  
